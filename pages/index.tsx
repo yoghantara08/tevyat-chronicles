780d1977943f1103b65ec9d99dfc68a25b9bfc0d
Yoghantara <gyoghantara@gmail.com>
2023-02-26 20:30:26 +0800

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
+import Welcome from "@/components/card/Welcome";
+import ResetTime from "@/components/countdown/ResetTime";
+import DailyFarming from "@/components/farming/DailyFarming";
+import YoutubeVideos from "@/components/youtube/YoutubeVideos";
+
 const Home = () => {
-  return <h1 className="text-2xl">Hello World</h1>;
+  return (
+    <section className="grid lg:grid-cols-3 w-full gap-5 text-gray-200 mb-5">
+      <div className="grid lg:grid-cols-2 lg:col-span-2 gap-5 h-fit">
+        <Welcome />
+        <ResetTime />
+        <DailyFarming />
+      </div>
+      <YoutubeVideos />
+    </section>
+  );
 };
 
 export default Home;
